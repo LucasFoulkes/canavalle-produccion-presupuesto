24f215603aa0a7ca9c81c8b1cb350d8e44bbf9e8
unknown <lucasfoulkes@gmail.com>
2025-06-14 03:16:11 -0500

    fix build bug

--- src/pages/Authentication.tsx
+++ src/pages/Authentication.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react';
 import { InputOTP, InputOTPGroup, InputOTPSlot } from "@/components/ui/input-otp";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
 import { useSupabase } from '@/hooks/useSupabase';
 import { useNavigate } from 'react-router-dom';
 
