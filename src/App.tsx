a1e914c170fa87721634f0961fc0d52f9ae3da6e
unknown <lucasfoulkes@gmail.com>
2025-06-18 18:51:54 -0500

    add font

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,14 @@ import {
 } from "@/components/ui/input-otp"
 
 function App() {
+  const maxLength = 6
   return (
-    <div className="flex justify-center items-center min-h-screen flex-col gap-2">
-      <InputOTP maxLength={6}>
-        <InputOTPGroup >
-          {/* <InputOTPGroup className='border-2 border-zinc-200 rounded-lg'> */}
-          <InputOTPSlot index={0} />
-          <InputOTPSlot index={1} />
-          <InputOTPSlot index={2} />
-          <InputOTPSlot index={3} />
-          <InputOTPSlot index={4} />
-          <InputOTPSlot index={5} />
+    <div className="flex justify-center items-center min-h-screen flex-col gap-4">
+      <InputOTP maxLength={maxLength}>
+        <InputOTPGroup>
+          {Array.from({ length: maxLength }, (_, i) =>
+            <InputOTPSlot key={i} index={i} className='size-14 border-zinc-400' />
+          )}
         </InputOTPGroup>
       </InputOTP>
       <div className="text-center text-sm">
