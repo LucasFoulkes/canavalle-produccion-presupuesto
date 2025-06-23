e79fab432da9992fd7d9aa000b2eaa61f696c792
unknown <lucasfoulkes@gmail.com>
2025-06-22 20:33:59 -0500

    fix import bug

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
-import Fincas from './pages/fincas'
+import Fincas from './pages/Fincas'
 import Bloques from './pages/Bloques'
 import Variedades from './pages/Variedades'
 
