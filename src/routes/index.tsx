3bc748631bb03f3a136669818c2827ccc01d7ed7
unknown <lucasfoulkes@gmail.com>
2025-07-19 11:02:29 -0500

    RESET

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,9 @@
 import { createFileRoute } from '@tanstack/react-router'
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from "@/components/ui/input-otp"
 
 export const Route = createFileRoute('/')({
   component: Index,
@@ -6,8 +11,17 @@ export const Route = createFileRoute('/')({
 
 function Index() {
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
+    <div >
+      <InputOTP maxLength={6}>
+        <InputOTPGroup>
+          <InputOTPSlot index={0} />
+          <InputOTPSlot index={1} />
+          <InputOTPSlot index={2} />
+          <InputOTPSlot index={3} />
+          <InputOTPSlot index={4} />
+          <InputOTPSlot index={5} />
+        </InputOTPGroup>
+      </InputOTP>
     </div>
   )
 }
