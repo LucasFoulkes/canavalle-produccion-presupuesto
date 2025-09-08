020b53879ee8668fa6e47d042ed68447ca58f536
unknown <lucasfoulkes@gmail.com>
2025-09-07 22:04:28 -0500

    add basich shadcn + tanstack react router + dexie + pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+
+
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
+  plugins: [tanstackRouter({
+    target: 'react',
+    autoCodeSplitting: true,
+  }), react(), tailwindcss(), VitePWA({
     registerType: 'autoUpdate',
     injectRegister: false,
 
@@ -32,5 +41,14 @@ export default defineConfig({
       suppressWarnings: true,
       type: 'module',
     },
-  })],
+  })
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
+
+
+
